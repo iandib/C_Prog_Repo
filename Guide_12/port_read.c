6592ad7b8d249ccceb8df951e9a08df05bee128b
iandib <ian.dib@outlook.com>
2024-11-02 13:14:32 -0300

    Simplified the logic in port_read.c

--- Guide_12/port_read.c
+++ Guide_12/port_read.c
@@ -58,38 +58,34 @@ void port_read(void)
     // Loop through each bit and set corresponding GPIO pin value based on bit state
     for (int i = 0; i < 8; i++)
     {
-        // Check the state of the current bit
-        if (port_bits[i] == 1 || port_bits[i] == 0)
+        // Try to open the file for the current GPIO pin; 
+        handle = fopen(gpio_paths[i], "w");
+        if (handle == NULL)
         {
-            // Try to open the file for the current GPIO pin; 
-            handle = fopen(gpio_paths[i], "w");
-            if (handle == NULL)
-            {
-                printf("Error: could not open file for pin %d \n", i);
-                exit(1);
-            }
-
-            // Print message if the file was opened successfully
-            else
-            {
-                printf("File %s opened successfully \n", gpio_paths[i]);
-            }
-
-            // Write '1' if bit is set, '0' otherwise
-            if (fputc(port_bits[i] ? '1' : '0', handle) == -1)
-            {
-                printf("Error: could not write to file for pin %d \n", i);
-                exit(1);
-            }
-
-            // Print message if the pin was updated successfully
-            else
-            {
-                printf("Pin %d updated successfully \n", i);
-            }
-            
-            // Close the file after updating the pin
-            fclose(handle);
+            printf("Error: could not open file for pin %d \n", i);
+            exit(1);
         }
+
+        // Print message if the file was opened successfully
+        else
+        {
+            printf("File %s opened successfully \n", gpio_paths[i]);
+        }
+
+        // Write '1' if bit is set, '0' otherwise
+        if (fputc(port_bits[i] ? '1' : '0', handle) == -1)
+        {
+            printf("Error: could not write to file for pin %d \n", i);
+            exit(1);
+        }
+
+        // Print message if the pin was updated successfully
+        else
+        {
+            printf("Pin %d updated successfully \n", i);
+        }
+        
+        // Close the file after updating the pin
+        fclose(handle);
     }
 }
